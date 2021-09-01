c06f6c5cb87063625262a539feedf43528757be6
Alexander Lalejini <amlalejini@gmail.com>
2021-09-01 16:36:34 -0400

    remove cookiecutter example code from web.cpp

--- source/web.cpp
+++ source/web.cpp
@@ -9,40 +9,40 @@
 #include "emp/web/web.hpp"
 
 #include "directed-digital-evolution/config_setup.hpp"
-#include "directed-digital-evolution/example.hpp"
-#include "directed-digital-evolution/ExampleConfig.hpp"
+// #include "directed-digital-evolution/example.hpp"
+// #include "directed-digital-evolution/ExampleConfig.hpp"
 
 namespace UI = emp::web;
 
 UI::Document doc("emp_base");
 
-directed_digital_evolution::Config cfg;
+// directed_digital_evolution::Config cfg;
 
 int main()
 {
   doc << "<h1>Hello, browser!</h1>";
 
   // Set up a configuration panel for web application
-  setup_config_web(cfg);
-  cfg.Write(std::cout);
-  emp::prefab::ConfigPanel example_config_panel(cfg);
-  example_config_panel.ExcludeSetting("SUPER_SECRET");
-  example_config_panel.SetRange("SEED", "-1", "100", "1");
-  doc << example_config_panel;
+  // setup_config_web(cfg);
+  // cfg.Write(std::cout);
+  // emp::prefab::ConfigPanel example_config_panel(cfg);
+  // example_config_panel.ExcludeSetting("SUPER_SECRET");
+  // example_config_panel.SetRange("SEED", "-1", "100", "1");
+  // doc << example_config_panel;
 
   // An example to show how the Config Panel could be used
   // to control the color of text in an HTML text area
   UI::TextArea example_area("example_area");
-  example_area.SetSize(cfg.SIZE(), cfg.SIZE());
-  example_config_panel.SetOnChangeFun([](const std::string & setting, const std::string & value){
-    UI::TextArea example_area = doc.TextArea("example_area");
-    example_area.SetCSS("color", cfg.COLOR());
-    example_area.Redraw();
-  });
+  // example_area.SetSize(cfg.SIZE(), cfg.SIZE());
+  // example_config_panel.SetOnChangeFun([](const std::string & setting, const std::string & value){
+  //   UI::TextArea example_area = doc.TextArea("example_area");
+  //   example_area.SetCSS("color", cfg.COLOR());
+  //   example_area.Redraw();
+  // });
 
   doc << example_area;
 
   std::cout << "Hello, console!" << "\n";
 
-  return example();
+  return 0;
 }
