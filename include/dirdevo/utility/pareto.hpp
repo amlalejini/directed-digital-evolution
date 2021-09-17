2e671a82ed1cda66358814fe0c30a52f426e2718
Alexander Lalejini <amlalejini@gmail.com>
2021-09-17 14:00:02 -0400

    implement find pareto front function

--- include/dirdevo/utility/pareto.hpp
+++ include/dirdevo/utility/pareto.hpp
@@ -18,4 +18,43 @@ namespace dirdevo {
     return !equal; // if a and b are not equal, a must dominate b (if b were ever greater than a, we would have returned false already)
   }
 
+    /// Each vector<double> in score table represents a single candidate's scores on a set of goals/objectives
+  emp::vector<size_t> find_pareto_front(const emp::vector< emp::vector<double> >& score_table) {
+
+    const size_t num_candidates = score_table.size();
+
+    emp::vector<size_t> all_candidates(num_candidates);
+    std::iota(
+      all_candidates.begin(),
+      all_candidates.end(),
+      0
+    );
+
+    // -- Find the pareto front --
+    // Initialize the front with the first candidate
+    emp::vector<size_t> front({all_candidates[0]});
+    for (size_t cand_i = 1; cand_i < all_candidates.size(); ++cand_i) {
+      const size_t cur_candidate_id = all_candidates[cand_i];
+      bool dominated = false; // tracks whether this candidate has been dominated by anything on the front
+      for (int front_i = 0; front_i < (int)front.size(); ++front_i) {
+        const size_t front_id = front[front_i];
+        emp_assert(score_table[cur_candidate_id].size() == score_table[front_id].size());
+        // note, this could be made more efficient (by computing sharing domination comparisons)
+        if (dirdevo::dominates(score_table[cur_candidate_id], score_table[front_id])) {
+          // candidate dominates member of the front, remove member of the front
+          std::swap(front[front_i], front[front.size()-1]); // Put dominated member of the front in the back
+          front.pop_back(); // Remove dominated member of the front
+          --front_i; // need to look at this index again
+        } else if (dirdevo::dominates(score_table[front_id], score_table[cur_candidate_id])) {
+          dominated = true;
+          break;
+        }
+      }
+      if (!dominated) front.emplace_back(cur_candidate_id);
+    }
+
+    return front;
+  }
+
+
 }
--- tests/pareto.cpp
+++ tests/pareto.cpp
@@ -4,12 +4,14 @@
 
 #include <unordered_set>
 
+#include "emp/math/Random.hpp"
+#include "emp/math/random_utils.hpp"
 #include "emp/datastructs/vector_utils.hpp"
 
 #include "dirdevo/utility/pareto.hpp"
 
 
-TEST_CASE("Test pareto utilities", "[utility][pareto][dominate]")
+TEST_CASE("Test dirdevo::dominates", "[utility][pareto][dominates]")
 {
 
   // No matter where we start, neither a nor b dominate one another
@@ -49,5 +51,74 @@ TEST_CASE("Test pareto utilities", "[utility][pareto][dominate]")
   CHECK(!dirdevo::dominates(b, a));
   CHECK(!dirdevo::dominates(a, b, 2));
   CHECK(!dirdevo::dominates(b, a, 2));
+}
+
+TEST_CASE("Test dirdevo::find_pareto_front", "[utility][pareto][find_pareto_front]") {
+  using set_t = std::unordered_set<size_t>;
+
+  emp::Random random(2);
+
+  // everything is part of the front
+  emp::vector< emp::vector<double> > score_table({
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1},
+  });
+  emp::vector<size_t> front = dirdevo::find_pareto_front(score_table);
+  CHECK(front.size() == 3);
+  CHECK(set_t(front.begin(), front.end()) == set_t({0,1,2}));
+
+  // 1 dominates all
+  score_table = {
+    {1, 0, 0},
+    {1, 1, 1},
+    {0, 0, 1},
+  };
+  front = dirdevo::find_pareto_front(score_table);
+  CHECK(front.size() == 1);
+  CHECK(set_t(front.begin(), front.end()) == set_t({1}));
+
+  // everything is part of the front
+  score_table = {
+    {1, 0},
+    {0, 1},
+    {0.75, 0.75},
+    {0.5, 0.8},
+    {0.8, 0.5},
+    {0.1, 0.9},
+    {0.9, 0.05},
+    {1, 0}
+  };
+  emp::Shuffle(random, score_table);
+  front = dirdevo::find_pareto_front(score_table);
+  CHECK(front.size() == score_table.size());
+  CHECK(set_t(front.begin(), front.end()) == set_t({0,1,2,3,4,5,6,7}));
+
+  // emp::vector< emp::vector<double> > correct_front_values(score_table);
+  // create a bunch of dominated values and add to the score vector
+  size_t trials = 100;
+  size_t num_extra_values = 10000;
+  for (size_t trial_i = 0; trial_i < trials; ++trial_i) {
+    emp::vector< emp::vector<double> > trial_score_table(score_table);
+    for (size_t val_i = 0; val_i < num_extra_values; ++val_i) {
+      // Pick a random obj vector
+      size_t front_id = random.GetUInt(score_table.size());
+      double obj0 = score_table[front_id][0] - random.GetDouble();
+      double obj1 = score_table[front_id][1] - random.GetDouble();
+      trial_score_table.emplace_back(emp::vector<double>({obj0, obj1}));
+    }
+    emp::Shuffle(random, trial_score_table);
+    auto trial_front = dirdevo::find_pareto_front(trial_score_table);
+    emp::vector< emp::vector<double> > trial_front_scores;
+    for (size_t id : trial_front) {
+      trial_front_scores.emplace_back(
+        trial_score_table[id]
+      );
+    }
+    CHECK(trial_front.size() == score_table.size());
+    for (const auto& scores : score_table) {
+      CHECK(emp::Has(trial_front_scores, scores));
+    }
+  }
 
 }
