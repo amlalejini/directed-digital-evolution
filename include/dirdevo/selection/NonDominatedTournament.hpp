99ad59bdad7ef638506ffabed99d1534f98f0f47
Alexander Lalejini <amlalejini@gmail.com>
2021-10-30 17:47:33 -0400

    Fix NonDominatedTournament selection

--- include/dirdevo/selection/NonDominatedTournament.hpp
+++ include/dirdevo/selection/NonDominatedTournament.hpp
@@ -18,6 +18,8 @@ struct NonDominatedTournamentSelect : public BaseSelect {
   emp::Random& random;    ///< When front size does not evenly fit into selected, use random to determine which things in the front fill the gap
   size_t tournament_size;
 
+  emp::vector<size_t> candidate_entrants;
+
   emp::vector< emp::vector<double> > score_table;
 
   NonDominatedTournamentSelect(
@@ -31,6 +33,7 @@ struct NonDominatedTournamentSelect : public BaseSelect {
   {  }
 
   emp::vector<size_t>& operator()(size_t n) override {
+    // std::cout << "---Running NDT---" << std::endl;
     selected.resize(n, 0);
     const size_t num_candidates = score_fun_sets.size();
     emp_assert(num_candidates > 0);
@@ -46,31 +49,68 @@ struct NonDominatedTournamentSelect : public BaseSelect {
       }
     }
 
+    // std::cout << "Score table:" << std::endl;
+    // for (size_t i = 0; i < score_table.size(); ++i) {
+    //   if (i) std::cout << "\n";
+    //   std::cout << " [" << i << "]: ";
+    //   for (size_t j = 0; j < score_table[i].size(); ++j) {
+    //     if (j) std::cout << " ";
+    //     std::cout << score_table[i][j];
+    //   }
+    // }
+    // std::cout << std::endl;
+
     // the entries for each tournament
+    candidate_entrants.resize(num_candidates);
+    emp_assert(tournament_size <= candidate_entrants.size());
+    std::iota(
+      candidate_entrants.begin(),
+      candidate_entrants.end(),
+      0
+    );
     emp::vector<size_t> entries(tournament_size);
     emp::vector< emp::vector<double> > entries_score_table(tournament_size, emp::vector<double>(obj_cnt));
     // run enough tournaments to get n winners
     size_t winners = 0;
     while (winners < n) {
+      // std::cout << "  -- running a tournament --" << std::endl;
       // form a tournament
-      std::generate(
-        entries.begin(),
-        entries.end(),
-        [this, num_candidates]() { return random.GetUInt(num_candidates); }
+      emp::Shuffle(random, candidate_entrants);
+      std::copy(
+        candidate_entrants.begin(),
+        candidate_entrants.begin()+tournament_size,
+        entries.begin()
       );
+
+      // std::cout << "  Entries: " << entries << std::endl;
+
       // copy score vectors of entrants into the tournament entry score table
       for (size_t i = 0; i < tournament_size; ++i) {
         const size_t entry_id = entries[i];
         std::copy(
-          entries_score_table[i].begin(),
-          entries_score_table[i].end(),
-          score_table[entry_id].begin()
+          score_table[entry_id].begin(),
+          score_table[entry_id].end(),
+          entries_score_table[i].begin()
         );
         emp_assert(score_table[entry_id] == entries_score_table[i], score_table, entries_score_table);
       }
 
+      // std::cout << "  Entries score table: " << std::endl;
+      // for (size_t i = 0; i < entries.size(); ++i) {
+      //   if (i) std::cout << "\n";
+      //   std::cout << "   [" << i << "(" << entries[i] << ")]: ";
+      //   for (size_t j = 0; j < entries_score_table[i].size(); ++j) {
+      //     if (j) std::cout << " ";
+      //     std::cout << entries_score_table[i][j];
+      //   }
+      // }
+      // std::cout << std::endl;
+
+
       // compute the pareto front of the entries
       emp::vector<size_t> tournament_front(dirdevo::find_pareto_front(entries_score_table));
+
+      // std::cout << "  Winners: " << tournament_front << std::endl;
 
       // select the winner(s), until we've selected enough things or until we've selected everything from this tournament
       for (size_t i = 0; i < tournament_front.size() && winners < selected.size(); ++i) {
--- tests/selection.cpp
+++ tests/selection.cpp
@@ -10,48 +10,86 @@
 #include "dirdevo/selection/Lexicase.hpp"
 #include "dirdevo/selection/NonDominatedElite.hpp"
 #include "dirdevo/selection/Tournament.hpp"
-#include "dirdevo/selection/Roulette.hpp"
+#include "dirdevo/selection/NonDominatedTournament.hpp"
 
-TEST_CASE("Test Elite Selection", "[selection][elite]")
-{
-  // Create some scores
-  emp::vector<double> scores = {/*0:*/ 8, /*1:*/ 128, /*2:*/ 2, /*3:*/ 32, /*4:*/ 16, /*5:*/ 4};
-  emp::vector<size_t> results;
-  std::unordered_set<size_t> results_set;
+TEST_CASE("Test NonDominatedTournamentSelection", "[selection][ndt]") {
+  using score_fun_t = std::function<double(void)>;
 
-  SECTION("emp::vector<size_t> EliteSelect(const emp::vector<double>&, size_t, size_t)") {
+  constexpr size_t seed = 2;
 
-    results = dirdevo::EliteSelect(scores, 1, 1);
-    CHECK(results==emp::vector<size_t>({1}));
-
-    results = dirdevo::EliteSelect(scores, 1, 100);
-    CHECK(results==emp::vector<size_t>(100, 1));
-
-    results = dirdevo::EliteSelect(scores, 3, 1);
-    CHECK(results.size()==3);
-    CHECK(emp::Has(results, (size_t)1));
-    CHECK(emp::Has(results, (size_t)3));
-    CHECK(emp::Has(results, (size_t)4));
-
-    results = dirdevo::EliteSelect(scores, 3, 10);
-    CHECK(results.size()==30);
-    results_set.clear();
-    results_set.insert(results.begin(), results.end());
-    CHECK(results_set.size()==3);
-    CHECK(results_set==std::unordered_set<size_t>({1,3,4}));
-
-    results = dirdevo::EliteSelect(scores, 5, 2);
-    CHECK(results.size()==10);
-    results_set.clear();
-    results_set.insert(results.begin(), results.end());
-    CHECK(results_set.size()==5);
-    CHECK(results_set==std::unordered_set<size_t>({1,3,4,0,5}));
-    CHECK(emp::Count(results, (size_t)1)==2);
-
+  emp::vector< emp::vector<score_fun_t> > score_fun_sets;
+  emp::vector< emp::vector<double> > scores{
+    /* 0= */ {1.0, 1.0, 1.0},
+    /* 1= */ {1.0, 0.0, 0.0},
+    /* 2= */ {0.0, 1.0, 0.0},
+    /* 3= */ {0.0, 0.0, 1.0},
+    /* 4= */ {0.0, 0.0, 0.0},
+    /* 5= */ {0.0, 2.0, 0.0},
+    /* 6= */ {2.0, 0.0, 0.0},
+    /* 7= */ {0.0, 0.0, 2.0}
+  };
+  for (size_t i = 0; i < scores.size(); ++i) {
+    score_fun_sets.emplace_back();
+    for (size_t j = 0; j < scores[i].size(); ++j) {
+      score_fun_sets[i].emplace_back(
+        [i,j,&scores](){return scores[i][j];}
+      );
+    }
   }
 
-  SECTION("void EliteSelect(emp::vector<size_t>&, const emp::vector<double>&, size_t)") {
-    // TODO
-  }
+  emp::Random random(seed);
+
+  dirdevo::NonDominatedTournamentSelect ndt_4(
+    score_fun_sets,
+    random,
+    8
+  );
+
+  std::cout << ndt_4(3) << std::endl;
+
 
 }
+
+// TEST_CASE("Test Elite Selection", "[selection][elite]")
+// {
+//   // Create some scores
+//   emp::vector<double> scores = {/*0:*/ 8, /*1:*/ 128, /*2:*/ 2, /*3:*/ 32, /*4:*/ 16, /*5:*/ 4};
+//   emp::vector<size_t> results;
+//   std::unordered_set<size_t> results_set;
+
+//   SECTION("emp::vector<size_t> EliteSelect(const emp::vector<double>&, size_t, size_t)") {
+
+//     results = dirdevo::EliteSelect(scores, 1, 1);
+//     CHECK(results==emp::vector<size_t>({1}));
+
+//     results = dirdevo::EliteSelect(scores, 1, 100);
+//     CHECK(results==emp::vector<size_t>(100, 1));
+
+//     results = dirdevo::EliteSelect(scores, 3, 1);
+//     CHECK(results.size()==3);
+//     CHECK(emp::Has(results, (size_t)1));
+//     CHECK(emp::Has(results, (size_t)3));
+//     CHECK(emp::Has(results, (size_t)4));
+
+//     results = dirdevo::EliteSelect(scores, 3, 10);
+//     CHECK(results.size()==30);
+//     results_set.clear();
+//     results_set.insert(results.begin(), results.end());
+//     CHECK(results_set.size()==3);
+//     CHECK(results_set==std::unordered_set<size_t>({1,3,4}));
+
+//     results = dirdevo::EliteSelect(scores, 5, 2);
+//     CHECK(results.size()==10);
+//     results_set.clear();
+//     results_set.insert(results.begin(), results.end());
+//     CHECK(results_set.size()==5);
+//     CHECK(results_set==std::unordered_set<size_t>({1,3,4,0,5}));
+//     CHECK(emp::Count(results, (size_t)1)==2);
+
+//   }
+
+//   SECTION("void EliteSelect(emp::vector<size_t>&, const emp::vector<double>&, size_t)") {
+//     // TODO
+//   }
+
+// }
