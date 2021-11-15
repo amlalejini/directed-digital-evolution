cdc85eaeb743511b9f59cc23cb62ff44d11bb36c
Alexander Lalejini <amlalejini@gmail.com>
2021-11-15 18:25:08 -0500

    draft 2021-11-15 ec experiment

---
+++ experiments/2021-11-15-ec/README.md
@@ -0,0 +1,5 @@
+# 2021-11-15 - Evolutionary computing experiment
+
+This experiment investigates the relative performance of the selection schemes used in our directed evolution model in an evolutionary computing context (for which most of them were designed).
+
+From 2021-11-11-selection experiment, median approximate number of generations elapsed ~57,000. So, for this experiment, I'll run populations for 55,000 generations.
---
+++ experiments/2021-11-15-ec/hpcc/job-gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+REPLICATES=50
+EXP_SLUG=2021-11-12-time
+
+SCRATCH_EXP_DIR=/mnt/scratch/lalejini/data/dirdevo
+HOME_EXP_DIR=/mnt/home/lalejini/devo_ws/directed-digital-evolution/experiments
+
+DATA_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}
+JOB_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}/jobs
+CONFIG_DIR=${HOME_EXP_DIR}/${EXP_SLUG}/hpcc/config
+
+python3 gen-sub.py --data_dir ${DATA_DIR}  --config_dir ${CONFIG_DIR} --replicates ${REPLICATES} --job_dir ${JOB_DIR}
