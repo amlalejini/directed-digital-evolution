043eea216a1b64cb886781d0267bc47fd8ff8f14
Alexander Lalejini <amlalejini@gmail.com>
2021-11-30 16:20:58 -0500

    draft job scripts for 2021-11-30 aligned tasks experiment

---
+++ experiments/2021-11-30-aligned-tasks/README.md
@@ -0,0 +1,3 @@
+# 2021-11-30 Aligned tasks experiment
+
+Configure environment such that all population-level tasks are also beneficial at the individual-level.
---
+++ experiments/2021-11-30-aligned-tasks/hpcc/job-gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+REPLICATES=50
+EXP_SLUG=2021-11-30-aligned-tasks
+
+SCRATCH_EXP_DIR=/mnt/scratch/lalejini/data/dirdevo
+HOME_EXP_DIR=/mnt/home/lalejini/devo_ws/directed-digital-evolution/experiments
+
+DATA_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}
+JOB_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}/jobs
+CONFIG_DIR=${HOME_EXP_DIR}/${EXP_SLUG}/hpcc/config
+
+python3 gen-sub.py --data_dir ${DATA_DIR}  --config_dir ${CONFIG_DIR} --replicates ${REPLICATES} --job_dir ${JOB_DIR}
